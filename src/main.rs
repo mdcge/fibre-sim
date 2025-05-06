257fc9b16492eefee96ff64424af0588b9b30def
Max Generowicz <git-mdcg@my-post-office.net>
2025-05-06 17:34:34 +0100

    Connect nodes with lines in rendering

--- src/main.rs
+++ src/main.rs
@@ -13,12 +13,14 @@ fn main() {
 struct Simulation {
     nodes: Vec<Node>,
     k: f32,  // total spring constant
-    x0: f32  // rest length of springs
+    x0: f32,  // rest length of springs
+    dn: f32,  // display size for nodes
+    ds: f32,  // display size for springs
 }
 
 impl Simulation {
-    fn new(node_list: Vec<Node>, spring_constant: f32, rest_length: f32) -> Simulation {
-        Simulation { nodes: node_list, k: spring_constant, x0: rest_length }
+    fn new(node_list: Vec<Node>, spring_constant: f32, rest_length: f32, node_diameter: f32, spring_thickness: f32) -> Simulation {
+        Simulation { nodes: node_list, k: spring_constant, x0: rest_length, dn: node_diameter, ds: spring_thickness }
     }
 }
 
@@ -28,12 +30,19 @@ impl Simulation {
         let draw = app.draw();
         draw.background().color(BLACK);
 
-        for node in &self.nodes {
+        for (i, node) in self.nodes.iter().enumerate() {
             draw.ellipse()
                 .color(WHITE)
-                .w(10.0)
-                .h(10.0)
+                .w(self.dn)
+                .h(self.dn)
                 .x_y(node.r[0]*scale, node.r[1]*scale);
+            if i != (self.nodes.len() - 1) {
+                draw.line()
+                    .color(WHITE)
+                    .start(pt2(self.nodes[i].r[0]*scale, self.nodes[i].r[1]*scale))
+                    .end(pt2(self.nodes[i+1].r[0]*scale, self.nodes[i+1].r[1]*scale))
+                    .weight(self.ds);
+            }
         }
 
         draw.to_frame(app, &frame).unwrap();
@@ -41,7 +50,7 @@ impl Simulation {
 }
 
 fn model(_app: &App) -> Simulation {
-    Simulation::new(vec![Node::new(vec![-1.0, 0.0], 1.0), Node::new(vec![0.0, 0.0], 1.0), Node::new(vec![1.0, 0.0], 1.0)], 1.0, 0.0)
+    Simulation::new(vec![Node::new(vec![-1.0, 0.0], 1.0), Node::new(vec![0.0, 0.0], 1.0), Node::new(vec![1.0, 0.0], 1.0)], 1.0, 0.0, 20.0, 8.0)
 }
 
 // `update` is like `event` except that the only event it triggers on is clock ticks
