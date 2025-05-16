556210872cb6922701a7b1e4e46716c73592979b
Max Generowicz <git-mdcg@my-post-office.net>
2025-05-16 11:03:39 +0100

    Turn gravity & damping loop into iterator
    Small speedup!

--- src/state.rs
+++ src/state.rs
@@ -13,29 +13,32 @@ impl State {
     }
 
     pub fn step(&mut self, k: f32, x0: f32, g: f32, c: f32, dt: f32) {
-        self.forces = vec![Vec2::new(0.0, 0.0); self.forces.len()];
+        let length = self.forces.len();
+        self.forces = vec![Vec2::new(0.0, 0.0); length];
 
         // Spring force
-        for i in 1..self.nodes.len()-1 {
+        for i in 1..length-1 {
             let pre_spring_force = spring_force(&self.nodes[i], &self.nodes[i-1], k, x0);
             let post_spring_force = spring_force(&self.nodes[i], &self.nodes[i+1], k, x0);
             self.forces[i] += pre_spring_force + post_spring_force;
         }
 
         // Gravity and damping
-        for i in 1..self.nodes.len()-1 {
-            let gravity_force = self.nodes[i].m * Vec2::new(0.0, -g);
-            let damping_force = -c * self.nodes[i].v;
-            self.forces[i] += gravity_force + damping_force;
-        }
+        self.forces[1..length-1].iter_mut()
+                                .zip(&mut self.nodes[1..length-1])
+                                .for_each(|(force, node)| {
+                                    // Gravity and damping
+                                    let gravity_force = node.m * Vec2::new(0.0, -g);
+                                    let damping_force = -c * node.v;
+                                    *force += gravity_force + damping_force;
+                                    // Integration
+                                    let acc = *force / node.m;
+                                    let vel = node.v + acc*dt;
+                                    let pos = node.r + vel*dt;
+                                    node.r = pos;
+                                    node.v = vel;
+                                });
 
         // Integration
-        for i in 1..self.nodes.len()-1 {
-            let acc = self.forces[i] / self.nodes[i].m;
-            let vel = self.nodes[i].v + acc*dt;
-            let pos = self.nodes[i].r + vel*dt;
-            self.nodes[i].r = pos;
-            self.nodes[i].v = vel;
-        }
     }
 }
