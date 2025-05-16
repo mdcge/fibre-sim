cc9c8a3aa7db551ed699824ae9fe3f4c474dcda2
Max Generowicz <git-mdcg@my-post-office.net>
2025-05-16 10:38:31 +0100

    Remove useless update function for Node

--- src/node.rs
+++ src/node.rs
@@ -11,11 +11,4 @@ impl Node {
     pub fn new(pos: Vec2, vel: Vec2, mass: f32) -> Node {
         Node { r: pos, v: vel, m: mass }
     }
-
-    pub fn update(&mut self, k_force: Vec2, g_force: Vec2, c_force: Vec2, dt: f32) {
-        let total_force = k_force + g_force + c_force;
-        let acc = total_force / self.m;
-        self.v += acc*dt;
-        self.r += self.v*dt;
-    }
 }
